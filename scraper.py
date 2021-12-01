21d8e3b1e5c10093841ee8e192a5db8438030c03
Adeniyi Olaitan <adeniyi.olaitanhector@yahoo.com>
2021-12-01 12:27:06 +0100

    added files

--- scraper.py
+++ scraper.py
@@ -1,63 +1,33 @@
 import time
 import tweepy
+from tweepy.api import API
 # Perform OS Actons
-import os as _os
-# Import the environmental vaiables
-import dotenv as _dotenv
-# load the environmental variables
-_dotenv.load_dotenv()
 
 # Set the Keys and tokens to the environemntal variable
-API_KEY = _os.environ["TWITTER_API_KEY"]
-SECRET_KEY = _os.environ["TWITTER_API_SECRET_KEY"]
-ACCESS_TOKEN = _os.environ["ACCESS_TOKEN"]
-ACCESS_TOKEN_SECRET = _os.environ["ACCESS_TOKEN_SECRET"]
+API_KEY = "c6trv1ovCx37seB8LnKab3l87"
+SECRET_KEY = "8oBtQBPuamgoMK62ImWVR64LPE8D0K6gWcRwHocZJrKPpZsRlW"
+ACCESS_TOKEN = "1281228204523454464-ayOCJc3p2vIKWDwhDUeFRVm1jYxpXB"
+ACCESS_TOKEN_SECRET = "hDzOFI3ObN7gCcqVriB6TRe3dyno7S0Q60PCBmB20mjw9"
 
 # Connect to the tweepy Auth
 auth = tweepy.OAuthHandler(API_KEY, SECRET_KEY)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
+api = tweepy.API(auth, wait_on_rate_limit=True)
+search_words = "$Dwac"
+date_since = "2021-11-16"
 # Create a listener object
+search_words = "$Dwac"
+date_since = "2021-11-16"
 
+go = tweepy.Client(auth, wait_on_rate_limit=True)
+tweets = go.search_all_tweets(query=search_words, max_results=1, until_id=1461456950231801861)
 
-class MyStreamListener(tweepy.Stream):
-    def on_status(self, status):
-        call = status.text
-        call = call.lower()
-        print(status.text)
-        if ("$dwac" in call and status.favorited == False):
-            
-            message =" hey, Did you see this $LMACA rumor? Could be a similar play https://twitter.com/Spacul8r/status/1455624133908238343?s=20"
-            print(message)
-            #api.update_status(status=message,in_reply_to_status_id = status.id, auto_populate_reply_metadata=True)
-            api.create_favorite(status.id)
-            self.disconnect()
-            
-    def disconnect(self):
-        return super().disconnect()
-        print("disconnected")
-    def on_connect(self):
-        print("Connected")
-
-    def on_error(self, status_code):
-        if status_code == 420:
-            # returning False in on_error disconnects the stream
-            return False
-
-        # returning non-False reconnects the stream, with backoff.
-
-# Create API object
-def main():
-    while True:
-        print("connecting")
-        global api
-        api = tweepy.API(auth)
-        myStreamListener = MyStreamListener(API_KEY, SECRET_KEY,ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        myStreamListener.filter(track=['$Dwac'])
-        print("done")
-        
-    
-if __name__ == '__main__':
-    main()
-    
-
+# Iterate and print tweets
+for tweet in tweets:
+    print(tweet.text)
+    try:
+       
+        message = " hey, Did you see this $LMACA rumor? Could be a similar play https://twitter.com/Spacul8r/status/1455624133908238343?s=20"
+        #api.update_status(status=message, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
+    except:
+        print("Already Commented")
